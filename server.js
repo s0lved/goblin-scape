25072b3b35b8e39aacd67ad37ba76dadf04611fb
s0lved <141227654+s0lved@users.noreply.github.com>
2024-11-21 23:10:24 +1100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const randomstring = require("randomstring");
 const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000;
-const cors = require('cors');
-app.use(cors());
+const app = express();
 app.use(express.json());
 let data = []
 let sharedKey
@@ -23,6 +22,18 @@ if (!process.env.SHARED_KEY) {
 }
 
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+         return res.status(200).json({})
+    }
     if (!req.headers.authorization) {
         return res.status(401).json({
             error: 'No credentials sent!'
