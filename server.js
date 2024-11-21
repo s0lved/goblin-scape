e7832f32db8e5ada8a02292b1beaef1956ddb590
s0lved <141227654+s0lved@users.noreply.github.com>
2024-11-21 23:51:41 +1100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ if (!process.env.SHARED_KEY) {
 }
 
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     if (!req.headers.authorization) {
         return res.status(401).json({
             error: 'No credentials sent!'
