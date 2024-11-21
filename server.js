fd1925002c42431a8b73455895d6865ec75940ef
s0lved <141227654+s0lved@users.noreply.github.com>
2024-11-22 00:15:56 +1100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", 'https://goblinscape.net');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Content-type,Accept,X-Custom-Header');
+    res.header("Access-Control-Allow-Headers", 'Content-type,Authorization,Accept,X-Custom-Header');
      if (req.method === "OPTIONS") {
         return res.status(200).end();
     }
