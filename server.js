0d45719e927fe33a895c5aa4c60661bdabc25552
s0lved <141227654+s0lved@users.noreply.github.com>
2024-11-21 23:16:04 +1100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const randomstring = require("randomstring");
 const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000;
-const app = express();
 app.use(express.json());
 let data = []
 let sharedKey
