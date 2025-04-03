ae35f28ea10993eda15f4b765f64fe6e5d68d0a5
s0lved <141227654+s0lved@users.noreply.github.com>
2025-04-04 10:06:41 +1100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (!process.env.SHARED_KEY) {
 }
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://goblinscape.net');
+    res.header("Access-Control-Allow-Origin", 'https://api.goblinscape.net');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Content-type,Authorization,Accept,X-Custom-Header');
